2c142b7a7d91fcc011d7af3011139fb5c8ce52df
MQuaresma <miguelquaresma97@gmail.com>
2018-09-11 09:13:01 +0100

    Navigation bar text colors fixed

--- static/scripts/navbar.js
+++ static/scripts/navbar.js
@@ -3,7 +3,9 @@ $(window).scroll(scrollHandler);
 function scrollHandler(){
     if($(window).scrollTop() > 5){
         $("#website-navbar").addClass("scrolled_nav");
+        $("a").addClass("scrolled");
     }else{
         $("#website-navbar").removeClass("scrolled_nav");
+        $("a").removeClass("scrolled");
     }
 };
